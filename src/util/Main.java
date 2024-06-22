6d0502b3993e46237388ee3b316dc7ef0ee79680
silkwave <silkwave24@gmail.com>
2024-06-22 13:20:38 +0900

    init

---
+++ src/util/Main.java
@@ -0,0 +1,43 @@
+package util;
+
+public class Main {
+    public static void main(String[] args) {
+        // Test StringToHexDecimal and HexDecimalToString
+        String originalString = "Hello, World!";
+        String hexString = Util.StringToHexDecimal(originalString);
+        System.out.println("Original String: " + originalString);
+        System.out.println("Hexadecimal String: " + hexString);
+        System.out.println("Converted back to String: " + Util.HexDecimalToString(hexString));
+
+        // Test makePhoneNumber
+        String phoneNumber = "1234567890";
+        String formattedPhoneNumber = Util.makePhoneNumber(phoneNumber);
+        System.out.println("Original Phone Number: " + phoneNumber);
+        System.out.println("Formatted Phone Number: " + formattedPhoneNumber);
+
+        // Test showMemory
+        System.out.println("Memory Usage: " + Util.showMemory());
+
+        // Test ReplaceAt
+        String replaceAtExample = "Hello, World!";
+        String replacedString = Util.ReplaceAt(replaceAtExample, 7, 12, "Java");
+        System.out.println("Original String: " + replaceAtExample);
+        System.out.println("String after replacement: " + replacedString);
+
+        // Test stringRepeat
+        String repeatString = "Hello";
+        int repeatCount = 3;
+        String repeatedString = Util.stringRepeat(repeatString, repeatCount);
+        System.out.println("String to repeat: " + repeatString);
+        System.out.println("Repeated String: " + repeatedString);
+
+        // Test log
+        Util.log("This is a log message.");
+
+        // Test addDay and getDay
+        String currentDate = Util.getDay();
+        String dateAfterAddingDays = Util.addDay(currentDate, 5);
+        System.out.println("Current Date: " + currentDate);
+        System.out.println("Date after adding 5 days: " + dateAfterAddingDays);
+    }
+}
