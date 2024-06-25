c328521270361cd79c4c1cf48ffbb2b5453c4f27
silkwave <silkwave24@gmail.com>
2024-06-25 21:56:16 +0900

    111

--- src/App.java
+++ src/App.java
@@ -14,7 +14,14 @@ public class App {
         System.out.println("클래스 이름: " + mainInstance.getClass().getName());
 
         // 순수 클래스 이름 출력
-        System.out.println("클래스 이름: " + mainInstance.getClass().getSimpleName());        
+        System.out.println("클래스 이름: " + mainInstance.getClass().getSimpleName());  
+        
+        // 현재 실행 중인 스레드 가져오기
+        Thread currentThread = Thread.currentThread();
+        
+        // 스레드 이름 출력
+        System.out.println("현재 스레드 이름: " + currentThread.getName());       
+           
 
     }
 
